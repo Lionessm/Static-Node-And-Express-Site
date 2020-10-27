ec36636b4f96392f0eeef9bb4b7791b657f36581
Maria <maria.mihaila.17@gmail.com>
2020-10-27 20:20:17 +0200

    Final code commnets

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const app = express();
 
 app.set('view engine', 'pug');
 
-//use a static route and the express.static method to serve the static files located in the public folder
+// Static route and the express.static method used to serve the static files located in the public folder.
 app.use('/static', express.static('public'));
 
-//An "index" route (/) to render the "Home" page with the locals set to data.projects
+// An "index" route (/) to render the "Home" page with the locals set to data.projects.
 app.get('/', ( req, res ) =>{
     res.render('index', data);
 })
-//An "about" route (/about) to render the "About" page
+// An "about" route (/about) to render the "About" page.
 app.get('/about', (req, res) => {
     res.render('about');
 })
 
-//Dynamic "project" routes (/project/:id or /projects/:id) based on the id of the project that render a customized version of the Pug project template to show off each project. Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
+// Dynamic "project" routes (/project/:id or /projects/:id) based on the id of the project that render a customized version of the Pug project template to show off each project.
 app.get('/project/:id', (req, res) => {
     const projectId = req.params.id;
     const projectData = data.projects;
@@ -56,7 +56,7 @@ app.use(function(req,res){
     res.render('error', { reason: 'Status Code: 404', error: error });
 });
 
-//Finally, start your server. Your app should listen on port 3000, and log a string to the console that says which port the app is listening to.
+
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
 });
